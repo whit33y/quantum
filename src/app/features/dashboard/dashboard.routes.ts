7c79a30f9b9f560097b1219de0fee5d45bb1b326
whit33y <troppout@gmail.com>
2026-01-20 07:23:23 +0100

    feat: add routes to all features

--- src/app/features/dashboard/dashboard.routes.ts
+++ src/app/features/dashboard/dashboard.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const DASHBOARD_ROUTES: Routes = [];
+export const DASHBOARD_ROUTES: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./pages/dashboard-page/dashboard-page').then((p) => p.DashboardPage),
+    title: 'dashboard',
+  },
+  {
+    path: 'asset-details/:coinId',
+    loadComponent: () =>
+      import('./pages/asset-details-page/asset-details-page').then((p) => p.AssetDetailsPage),
+    title: 'asset details',
+  },
+];
--- src/app/features/portfolio/portfolio.routes.ts
+++ src/app/features/portfolio/portfolio.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const PORTFOLIO_ROUTES: Routes = [];
+export const PORTFOLIO_ROUTES: Routes = [
+  {
+    path: 'wallet',
+    loadComponent: () =>
+      import('./pages/portfolio-page/portfolio-page').then((p) => p.PortfolioPage),
+    title: 'portfolio',
+  },
+  {
+    path: 'watchlist',
+    loadComponent: () =>
+      import('./pages/watch-list-page/watch-list-page').then((p) => p.WatchListPage),
+    title: 'watchlist',
+  },
+];
