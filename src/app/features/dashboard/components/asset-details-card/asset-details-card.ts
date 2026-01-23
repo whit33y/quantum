ca5df886616949c93941835c11bd91f027dedefa
Jakub Bryska <troppout@gmail.com>
2026-01-23 13:49:56 +0100

    feat: add charts to asset-details-card

--- src/app/features/dashboard/components/asset-details-card/asset-details-card.ts
+++ src/app/features/dashboard/components/asset-details-card/asset-details-card.ts
@@ -1,19 +1,21 @@
 import { Component, input } from '@angular/core';
 import { DatePipe, UpperCasePipe, CurrencyPipe, DecimalPipe } from '@angular/common';
 import { CoinDetails } from '../../../../shared/models/coin-api.model';
-import {
-  Calendar,
-  Cpu,
-  Hash,
-  LucideAngularModule,
-  TrendingUp,
-  TrendingDown,
-  AlignLeft,
-} from 'lucide-angular';
+import { Calendar, Cpu, Hash, LucideAngularModule, TrendingUp, TrendingDown } from 'lucide-angular';
+import { BasicLineChart } from '../../../../shared/components/basic-line-chart/basic-line-chart';
+import { BasicBarChart } from '../../../../shared/components/basic-bar-chart/basic-bar-chart';
 
 @Component({
   selector: 'app-asset-details-card',
-  imports: [UpperCasePipe, LucideAngularModule, DatePipe, CurrencyPipe, DecimalPipe],
+  imports: [
+    UpperCasePipe,
+    LucideAngularModule,
+    DatePipe,
+    CurrencyPipe,
+    DecimalPipe,
+    BasicLineChart,
+    BasicBarChart,
+  ],
   templateUrl: './asset-details-card.html',
   styleUrl: './asset-details-card.css',
 })
@@ -25,4 +27,8 @@ export class AssetDetailsCard {
   readonly TrendingDownIcon = TrendingDown;
 
   coinDetails = input<CoinDetails>();
+  lineChartData = input<number[]>([]);
+  lineChartLabels = input<string[]>([]);
+  chartData = input<number[]>([]);
+  chartLabel = input<string[]>([]);
 }
--- src/app/features/dashboard/pages/asset-details-page/asset-details-page.ts
+++ src/app/features/dashboard/pages/asset-details-page/asset-details-page.ts
@@ -1,6 +1,6 @@
-import { Component, effect, inject, input, signal } from '@angular/core';
+import { Component, computed, effect, inject, input, signal, WritableSignal } from '@angular/core';
 import { CoinApiService } from '../../services/coin-api-service';
-import { CoinDetails } from '../../../../shared/models/coin-api.model';
+import { CoinDetails, MarketChart } from '../../../../shared/models/coin-api.model';
 import { AssetDetailsCard } from '../../components/asset-details-card/asset-details-card';
 
 @Component({
@@ -16,20 +16,82 @@ export class AssetDetailsPage {
   eff = effect(() => {
     const id = this.coinId();
     if (id) {
-      this.loadCoinDetails(id);
+      this.loadCoinDetails(id, this.coinDetails);
+      this.getMarketChart(id, '30', this.marketChart, 'usd');
     }
   });
 
   error = signal<string>('');
   coinDetails = signal<CoinDetails | undefined>(undefined);
-  loadCoinDetails(coinId: string) {
-    this.coinApiService.getCoinDetails(coinId).subscribe({
-      next: (response) => {
-        this.coinDetails.set(response);
-        console.log(response);
-      },
+  loadCoinDetails(
+    coinId: string,
+    targetSignal?: WritableSignal<CoinDetails | undefined>,
+    tickers?: boolean,
+    developer_data?: boolean,
+    localization?: boolean,
+    include_categories_details?: boolean,
+  ) {
+    this.coinApiService
+      .getCoinDetails(coinId, tickers, developer_data, localization, include_categories_details)
+      .subscribe({
+        next: (response) => {
+          if (targetSignal) {
+            targetSignal.set(response);
+          }
+        },
+        error: (err) => {
+          console.error(err);
+        },
+      });
+  }
+
+  chartLabels = ['1h', '24h', '7d', '14d', '30d'];
+
+  chartData = computed(() => {
+    const info = this.coinDetails();
+    if (!info?.market_data) return [];
+
+    return [
+      info.market_data.price_change_percentage_1h_in_currency['usd'] ?? 0,
+      info.market_data.price_change_percentage_24h_in_currency['usd'] ?? 0,
+      info.market_data.price_change_percentage_7d_in_currency['usd'] ?? 0,
+      info.market_data.price_change_percentage_14d_in_currency['usd'] ?? 0,
+      info.market_data.price_change_percentage_30d_in_currency['usd'] ?? 0,
+    ];
+  });
+
+  lineChartLabels = computed(() => {
+    const chart = this.marketChart();
+    if (!chart) return [];
+
+    return chart.prices.map((p) =>
+      new Date(p[0]).toLocaleDateString(undefined, {
+        month: 'short',
+        day: 'numeric',
+      }),
+    );
+  });
+
+  lineChartData = computed(() => {
+    const chart = this.marketChart();
+    if (!chart) return [];
+
+    return chart.prices.map((p) => p[1]);
+  });
+
+  marketChart = signal<MarketChart | undefined>(undefined);
+  errorMarketChart = signal<string>('');
+  getMarketChart(
+    coinName: string,
+    days: string,
+    targetSignal: WritableSignal<MarketChart | undefined>,
+    currency?: string,
+  ) {
+    this.coinApiService.getMarketChart(coinName, days, currency).subscribe({
+      next: targetSignal.set,
       error: (err) => {
         console.error(err);
+        this.errorMarketChart.set(err.message ?? 'Chart error');
       },
     });
   }
