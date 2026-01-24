5541dbf157aaebd9d49347ff725aa37bd54db9f8
whit33y <troppout@gmail.com>
2026-01-24 16:45:07 +0100

    feat: add styles to watch-list info

--- src/app/features/portfolio/components/watch-list-info/watch-list-info.ts
+++ src/app/features/portfolio/components/watch-list-info/watch-list-info.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { Eye, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-watch-list-info',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './watch-list-info.html',
   styleUrl: './watch-list-info.css',
 })
 export class WatchListInfo {
-
+  readonly Eye = Eye;
+  lastAddedCoin = input<string>();
+  watchListCount = input<number>();
 }
